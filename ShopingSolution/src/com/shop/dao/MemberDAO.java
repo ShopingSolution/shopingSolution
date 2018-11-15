a92400aafe5f4abe52e664a1a2e01f80b09e2a6e
Adam <Adam@QB52CTB>
2018-11-15 21:04:34 +0900

    template process #1

--- ShopingSolution/src/com/shop/dao/MemberDAO.java
+++ ShopingSolution/src/com/shop/dao/MemberDAO.java
@@ -1,15 +1,24 @@
 package com.shop.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import com.shop.dto.MemberVO;
 
 public interface MemberDAO {
-	public String selectIdNameWithEmail(String name, String email) throws SQLException;
-	public String confirmIdWithEmail(String id, String email) throws SQLException;
+	public String confirmIdByPwdFind(String id) throws SQLException;
 	public String selectPasswordById(String id) throws SQLException;
-	public void resetPassword(String id) throws SQLException;
 	
 	public MemberVO getMember(String id) throws SQLException;
+	
+	
+	public List<MemberVO> listMember() throws SQLException;
+	
+	public void confirmId(String id) throws SQLException;
+	public void modifyActivation(String id) throws SQLException;
+	public void modifyPassword(String id, String pwd) throws SQLException;
+	public void resetPassword(String id) throws SQLException;
+	public void registMember(MemberVO member) throws SQLException;
+	public void outMember(String id) throws SQLException;
 	
 }
