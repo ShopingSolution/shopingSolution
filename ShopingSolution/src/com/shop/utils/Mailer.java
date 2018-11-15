7b182f7cc6973bbc800686ac99b6c825effbd195
Adam <Adam@QB52CTB>
2018-11-15 21:04:59 +0900

    template #1

--- ShopingSolution/src/com/shop/utils/Mailer.java
+++ ShopingSolution/src/com/shop/utils/Mailer.java
@@ -1,12 +1,9 @@
 package com.shop.utils;
 
-import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 import javax.mail.Address;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
